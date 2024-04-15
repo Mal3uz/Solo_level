161ef39c8c7766dcf8dc54c9cea1088cdedfd3a3
Mal3uz <phuongnampham7823@gmail.com>
2024-04-15 22:42:36 +0700

    Killing the enemy and looting heart

--- Scenes/Enemies/enemy_mover.gd
+++ Scenes/Enemies/enemy_mover.gd
@@ -3,19 +3,28 @@ class_name enemy_movement
 
 
 
-enum player_states {MOVELEFT, MOVERIGHT, IDLE}
+enum enemy_states {MOVELEFT, MOVERIGHT, DEAD}
 var current_states 
 
+@onready var dead_anim = preload("res://Scenes/Effects/dead_fx.tscn")
+@onready var heart_loot = preload("res://Scenes/Interactables/heart.tscn")
 @export var speed = 10
+@export var health = 3
+
 var dir
 
 
 func _physics_process(delta):
+	if health <= 0:
+		current_states = enemy_states.DEAD
+	
 	match  current_states:
-		player_states.MOVELEFT:
+		enemy_states.MOVELEFT:
 			move_left()
-		player_states.MOVERIGHT:
+		enemy_states.MOVERIGHT:
 			move_right()
+		enemy_states.DEAD:
+			dead()
 	
 	move_and_slide()
 
@@ -27,9 +36,9 @@ func random_generation():
 func random_direction():
 	match dir:
 		0:
-			current_states = player_states.MOVERIGHT
+			current_states = enemy_states.MOVERIGHT
 		1:
-			current_states = player_states.MOVELEFT
+			current_states = enemy_states.MOVELEFT
 
 func move_right():
 	velocity = Vector2.RIGHT * speed
@@ -39,3 +48,17 @@ func move_left():
 	velocity = Vector2.LEFT * speed
 	$anim.play("move_left")
 
+func dead():
+	dead_animation()
+	queue_free()
+	
+func dead_animation():
+	var dead = dead_anim.instantiate()
+	dead.global_position = global_position
+	get_tree().get_root().add_child(dead)
+	loot_heart()
+
+func loot_heart():
+	var heart = heart_loot.instantiate()
+	heart.global_position = global_position
+	get_tree().get_root().add_child(heart)
--- Scripts/Enemy/enemy_1.gd
+++ Scripts/Enemy/enemy_1.gd
@@ -1,5 +1,6 @@
 extends enemy_movement
 
+
 func _ready():
 	random_generation()
 
@@ -7,3 +8,12 @@ func _ready():
 func _on_timer_timeout():
 	random_generation()
 	$Timer.start()
+
+
+
+
+
+func _on_hitbox_area_entered(area):
+	if area.is_in_group("Range"):
+		health -= 1
+		print(health)
